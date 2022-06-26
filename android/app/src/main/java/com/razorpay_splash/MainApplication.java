3669d36c74867de9a05adb59682ae441abb4f95a
saiashish9 <saiashish3760@gmail.com>
2022-06-27 00:49:40 +0530

    completed

--- android/app/src/main/java/com/razorpay_splash/MainApplication.java
+++ android/app/src/main/java/com/razorpay_splash/MainApplication.java
@@ -2,6 +2,9 @@ package com.razorpay_splash;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+import android.view.WindowManager;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -52,7 +55,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    // If you opted-in for the New Architecture, we enable the TurboModule system
+      // If you opted-in for the New Architecture, we enable the TurboModule system
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
